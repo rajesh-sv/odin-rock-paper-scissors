8e7ee5f26e1e3f8c0e9decc476fd968561b5ebb1
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2023-05-24 13:50:18 +0530

    Refactor playRound and game functions to be more readable

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 function getPlayerChoice() {
-  while(true) {
+  while (true) {
     const choice = prompt('Enter your choice (rock, paper or scissors)').toLowerCase().trim();
-    if(choice === "rock" || choice === "paper" || choice === "scissors") {
+    if (choice === "rock" || choice === "paper" || choice === "scissors") {
       return choice;
     }
     alert('Invalid Input! Please enter a valid choice.');
@@ -10,43 +10,46 @@ function getPlayerChoice() {
 
 function getComputerChoice() {
   random_choice = Math.floor(Math.random() * 3) + 1;
-  return random_choice === 1?
-          "rock":random_choice ===2?
-          "paper": "scissors";
+  return random_choice === 1 ?
+    "rock" : random_choice === 2 ?
+      "paper" : "scissors";
 }
 
 function playRound() {
   const playerChoice = getPlayerChoice();
   const computerChoice = getComputerChoice();
-  if(playerChoice === computerChoice) {
-    return [0, "Draw!"];
-  } else if(playerChoice === "rock") {
-    return computerChoice === "scissors"?
-            [1, "You Win! Rock beats Scissors"]:[-1, "You Lose! Paper beats Rock"];
-  } else if(playerChoice === "paper") {
-    return computerChoice === "rock"?
-            [1, "You Win! Paper beats Rock"]:[-1, "You Lose! Scissors beat Paper"];
+  if (playerChoice === computerChoice) {
+    return { draw: true, result: null, winningChoice: null, losingChoice: null };
+  } else if (playerChoice === "rock") {
+    return computerChoice === "scissors" ?
+      { draw: false, result: "Win", winningChoice: "Rock", losingChoice: "Scissors" } : { draw: false, result: "Lose", winningChoice: "Paper", losingChoice: "Rock" };
+  } else if (playerChoice === "paper") {
+    return computerChoice === "rock" ?
+      { draw: false, result: "Win", winningChoice: "Paper", losingChoice: "Rock" } : { draw: false, result: "Lose", winningChoice: "Scissors", losingChoice: "Paper" };
   } else {
-    return computerChoice === "paper"?
-            [1, "You Win! Scissors beat Paper"]:[-1, "You Lose! Rock beats Scissors"];
+    return computerChoice === "paper" ?
+      { draw: false, result: "Win", winningChoice: "Scissors", losingChoice: "Paper" } : { draw: false, result: "Lose", winningChoice: "Rock", losingChoice: "Scissors" };
   }
 }
 
-function game(playerChoice, computerChoice) {
+function game() {
   let playerWins = 0;
   let computerWins = 0;
-  for(let i=1; i<6; ++i) {
-    const [roundWinner, roundMessage] = playRound();
-    if(roundWinner === 1) {
-      playerWins += 1;
-    } else if(roundWinner === -1) {
-      computerWins += 1;
+
+  for (let i = 1; i < 6; ++i) {
+    const roundDetails = playRound();
+    if(!roundDetails.draw && roundDetails.result === "Win") {
+       ++playerWins;
+    } else {
+      ++computerWins;
     }
+    roundMessage = (roundDetails.draw) ? "Draw!" : `You ${roundDetails.result}! ${roundDetails.winningChoice} beats ${roundDetails.losingChoice}`;
     console.log(`ROUND ${i}: ${roundMessage}`);
   }
-  if(playerWins > computerWins) {
+
+  if (playerWins > computerWins) {
     console.log("Victory! The Force is with you.");
-  } else if(playerWins < computerWins) {
+  } else if (playerWins < computerWins) {
     console.log("Defeat! May the Force be with you.");
   } else {
     console.log("Draw! Master the Force.");
