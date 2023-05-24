ce05ad04d8d19b8b55061fbf3d5c6d29f76842ed
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2023-05-24 23:12:17 +0530

    Add UI for Rock Paper Scissors game

--- script.js
+++ script.js
@@ -1,11 +1,42 @@
-function getPlayerChoice() {
-  while (true) {
-    const choice = prompt('Enter your choice (rock, paper or scissors)').toLowerCase().trim();
-    if (choice === "rock" || choice === "paper" || choice === "scissors") {
-      return choice;
-    }
-    alert('Invalid Input! Please enter a valid choice.');
+let playerScore = 0;
+let computerScore = 0;
+
+const buttons = document.querySelectorAll('button');
+const roundDetailsDiv = document.querySelector('.round-details');
+const runningScoreDiv = document.querySelector('.running-score');
+const playerScoreDiv = document.querySelector('.player-score');
+const computerScoreDiv = document.querySelector('.computer-score');
+const gameEndDiv = document.querySelector('.game-end');
+
+buttons.forEach(button => button.addEventListener('click', () => playRound(button.value)))
+
+function playRound(playerChoice) {
+  if (isGameOver()) cleanUp();
+  const computerChoice = getComputerChoice();
+  let roundDetails;
+  if (playerChoice === computerChoice) {
+    roundDetails = { draw: true, result: null, winningChoice: null, losingChoice: null };
+  } else if (playerChoice === "rock") {
+    roundDetails = computerChoice === "scissors" ?
+      { draw: false, result: "Win", winningChoice: "Rock", losingChoice: "Scissors" } : { draw: false, result: "Lose", winningChoice: "Paper", losingChoice: "Rock" };
+  } else if (playerChoice === "paper") {
+    roundDetails = computerChoice === "rock" ?
+      { draw: false, result: "Win", winningChoice: "Paper", losingChoice: "Rock" } : { draw: false, result: "Lose", winningChoice: "Scissors", losingChoice: "Paper" };
+  } else {
+    roundDetails = computerChoice === "paper" ?
+      { draw: false, result: "Win", winningChoice: "Scissors", losingChoice: "Paper" } : { draw: false, result: "Lose", winningChoice: "Rock", losingChoice: "Scissors" };
   }
+  displayRoundDetails(roundDetails);
+}
+
+function cleanUp() {
+  roundDetailsDiv.textContent = "";
+  runningScoreDiv.textContent = "";
+  playerScoreDiv.textContent = "";
+  computerScoreDiv.textContent = "";
+  gameEndDiv.textContent = "";
+  playerScore = 0;
+  computerScore = 0;
 }
 
 function getComputerChoice() {
@@ -15,45 +46,41 @@ function getComputerChoice() {
       "paper" : "scissors";
 }
 
-function playRound() {
-  const playerChoice = getPlayerChoice();
-  const computerChoice = getComputerChoice();
-  if (playerChoice === computerChoice) {
-    return { draw: true, result: null, winningChoice: null, losingChoice: null };
-  } else if (playerChoice === "rock") {
-    return computerChoice === "scissors" ?
-      { draw: false, result: "Win", winningChoice: "Rock", losingChoice: "Scissors" } : { draw: false, result: "Lose", winningChoice: "Paper", losingChoice: "Rock" };
-  } else if (playerChoice === "paper") {
-    return computerChoice === "rock" ?
-      { draw: false, result: "Win", winningChoice: "Paper", losingChoice: "Rock" } : { draw: false, result: "Lose", winningChoice: "Scissors", losingChoice: "Paper" };
-  } else {
-    return computerChoice === "paper" ?
-      { draw: false, result: "Win", winningChoice: "Scissors", losingChoice: "Paper" } : { draw: false, result: "Lose", winningChoice: "Rock", losingChoice: "Scissors" };
+function displayRoundDetails(roundDetails) {
+  roundDetailsDiv.textContent = (roundDetails.draw) ? "Draw!" : `You ${roundDetails.result}! ${roundDetails.winningChoice} beats ${roundDetails.losingChoice}`;
+  updateRunningScore(roundDetails);
+  displayRunningScore();
+  if (isGameOver()) {
+    displayGameResults();
   }
 }
 
-function game() {
-  let playerWins = 0;
-  let computerWins = 0;
-
-  for (let i = 1; i < 6; ++i) {
-    const roundDetails = playRound();
-    if(!roundDetails.draw && roundDetails.result === "Win") {
-       ++playerWins;
+function updateRunningScore(roundDetails) {
+  if (!roundDetails.draw) {
+    if (roundDetails.result === "Win") {
+      ++playerScore;
     } else {
-      ++computerWins;
+      ++computerScore;
     }
-    roundMessage = (roundDetails.draw) ? "Draw!" : `You ${roundDetails.result}! ${roundDetails.winningChoice} beats ${roundDetails.losingChoice}`;
-    console.log(`ROUND ${i}: ${roundMessage}`);
-  }
-
-  if (playerWins > computerWins) {
-    console.log("Victory! The Force is with you.");
-  } else if (playerWins < computerWins) {
-    console.log("Defeat! May the Force be with you.");
-  } else {
-    console.log("Draw! Master the Force.");
   }
 }
 
-game();
+function displayRunningScore() {
+  runningScoreDiv.textContent = "Running Score";
+  playerScoreDiv.textContent = `Player: ${playerScore}`;
+  computerScoreDiv.textContent = `Computer: ${computerScore}`;
+}
+
+function displayGameResults() {
+  if (playerScore > computerScore) {
+    gameEndDiv.textContent = "Victory! The Force is with you."
+  } else if (playerScore < computerScore) {
+    gameEndDiv.textContent = "Defeat! May the Force be with you.";
+  } else {
+    gameEndDiv.textContent = "Draw! Master the Force.";
+  }
+}
+
+function isGameOver() {
+  return playerScore === 5 || computerScore === 5;
+}
