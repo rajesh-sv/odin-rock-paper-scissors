f0cffa2a23fce6962be6d28cc33b797bdbc4e368
rajesh-sv <125382031+rajesh-sv@users.noreply.github.com>
2023-09-03 18:36:27 +0530

    Remove redundant 'draw' property from roundDetails object

--- script.js
+++ script.js
@@ -15,16 +15,16 @@ function playRound(playerChoice) {
   const computerChoice = getComputerChoice();
   let roundDetails;
   if (playerChoice === computerChoice) {
-    roundDetails = { draw: true, result: null, winningChoice: null, losingChoice: null };
+    roundDetails = { result: "Draw", winningChoice: null, losingChoice: null };
   } else if (playerChoice === "rock") {
     roundDetails = computerChoice === "scissors" ?
-      { draw: false, result: "Win", winningChoice: "Rock", losingChoice: "Scissors" } : { draw: false, result: "Lose", winningChoice: "Paper", losingChoice: "Rock" };
+      { result: "Win", winningChoice: "Rock", losingChoice: "Scissors" } : { result: "Lose", winningChoice: "Paper", losingChoice: "Rock" };
   } else if (playerChoice === "paper") {
     roundDetails = computerChoice === "rock" ?
-      { draw: false, result: "Win", winningChoice: "Paper", losingChoice: "Rock" } : { draw: false, result: "Lose", winningChoice: "Scissors", losingChoice: "Paper" };
+      { result: "Win", winningChoice: "Paper", losingChoice: "Rock" } : { result: "Lose", winningChoice: "Scissors", losingChoice: "Paper" };
   } else {
     roundDetails = computerChoice === "paper" ?
-      { draw: false, result: "Win", winningChoice: "Scissors", losingChoice: "Paper" } : { draw: false, result: "Lose", winningChoice: "Rock", losingChoice: "Scissors" };
+      { result: "Win", winningChoice: "Scissors", losingChoice: "Paper" } : { result: "Lose", winningChoice: "Rock", losingChoice: "Scissors" };
   }
   displayRoundDetails(roundDetails);
 }
@@ -47,7 +47,7 @@ function getComputerChoice() {
 }
 
 function displayRoundDetails(roundDetails) {
-  roundDetailsDiv.textContent = (roundDetails.draw) ? "Draw!" : `You ${roundDetails.result}! ${roundDetails.winningChoice} beats ${roundDetails.losingChoice}`;
+  roundDetailsDiv.textContent = (roundDetails.result === "Draw") ? "Draw!" : `You ${roundDetails.result}! ${roundDetails.winningChoice} beats ${roundDetails.losingChoice}`;
   updateRunningScore(roundDetails);
   displayRunningScore();
   if (isGameOver()) {
@@ -56,13 +56,11 @@ function displayRoundDetails(roundDetails) {
 }
 
 function updateRunningScore(roundDetails) {
-  if (!roundDetails.draw) {
     if (roundDetails.result === "Win") {
       ++playerScore;
-    } else {
+    } else if (roundDetails.result === "Lose") {
       ++computerScore;
     }
-  }
 }
 
 function displayRunningScore() {
@@ -76,8 +74,6 @@ function displayGameResults() {
     gameEndDiv.textContent = "Victory! The Force is with you."
   } else if (playerScore < computerScore) {
     gameEndDiv.textContent = "Defeat! May the Force be with you.";
-  } else {
-    gameEndDiv.textContent = "Draw! Master the Force.";
   }
 }
 
